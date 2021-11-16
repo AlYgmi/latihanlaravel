0095debb27a085cf075649237e862812223eab0e
AlYgmi <nurrusulialadiyat@gmail.com>
2021-11-16 11:39:20 +0700

    akhirnya

---
+++ as
@@ -0,0 +1,35 @@
+[33mcommit e17a7c5580752ee4712c781c7851610eebfdf61b[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: AlYgmi <nurrusulialadiyat@gmail.com>
+Date:   Tue Nov 16 10:12:21 2021 +0700
+
+    coba test
+
+[33mcommit 0a0898d99d6cb1caf61cc85563e335bf3e464fbc[m
+Author: AlYgmi <nurrusulialadiyat@gmail.com>
+Date:   Mon Nov 15 12:36:34 2021 +0700
+
+    nambah lagi
+
+[33mcommit 73898c40a8a4004749285a5e250b8f148c6655dc[m
+Author: AlYgmi <nurrusulialadiyat@gmail.com>
+Date:   Fri Nov 12 12:50:18 2021 +0700
+
+    yes hampir selesai
+
+[33mcommit 329182e3b21a96296298c3c340af41253e6db1d4[m
+Author: AlYgmi <nurrusulialadiyat@gmail.com>
+Date:   Thu Nov 11 14:22:46 2021 +0700
+
+    nambah dikit
+
+[33mcommit eb333a184536fb9f1ad076a7a40f3b440647d4f8[m
+Author: AlYgmi <nurrusulialadiyat@gmail.com>
+Date:   Wed Nov 10 09:28:37 2021 +0700
+
+    baru lagi soalnya susah masuk ke localhostnya
+
+[33mcommit b88a774fc99a1d55c5b591ded3ea73537961227f[m
+Author: AlYgmi <nurrusulialadiyat@gmail.com>
+Date:   Wed Nov 10 09:26:16 2021 +0700
+
+    first commit
--- resources/views/jual.blade.php
+++ resources/views/jual.blade.php
@@ -8,65 +8,9 @@
 @section('contrast')
 <div class="container">
 <div class="row">
-  <div class="col">
-<div class="card" style="width: 18rem;">
-  <img src="https://sekolahpedia.id/medias/produk/b5887.jpg" class="card-img-top" alt="Lisensi Office A1 3656">
-  <div class="card-body">
-    <h5 class="card-title">Lisensi Office A1 3656</h5>
-    <p class="card-text">SMK ANGKASA 1 MARGAHAYU</p>
-    <a href="#" class="btn btn-primary">Beli</a>
-  </div>
+    <div class="card">
 </div>
 </div>
 
-<div class="col">
-<div class="card" style="width: 18rem;">
-  <img src="https://sekolahpedia.id/medias/produk/yvwkh.png" class="card-img-top" alt="Lisensi Mikrotik Level 4">
-  <div class="card-body">
-    <h5 class="card-title">Lisensi Mikrotik Level 4</h5>
-    <p class="card-text">SMK ANGKASA 1 MARGAHAYU</p>
-    <a href="#" class="btn btn-primary">Beli</a>
-  </div>
-  </div>
-</div>
-  
-  <div class="col">
-  <div class="card" style="width: 18rem;">
-  <img src="https://sekolahpedia.id/medias/produk/h6yvz.png" class="card-img-top" alt="MIKROTIK RB 941-2ND Haplite">
-  <div class="card-body">
-    <h5 class="card-title">MIKROTIK RB 941-2ND Haplite </h5>
-    <p class="card-text">SMK ANGKASA 1 MARGAHAYU</p>
-    <a href="#" class="btn btn-primary">Beli</a>
-</div>
-</div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="https://sekolahpedia.id/medias/produk/c4pzz.png" class="card-img-top" alt="JASA SERVICE LAPTOP/PC dan HP">
-  <div class="card-body">
-    <h5 class="card-title">JASA SERVICE LAPTOP/PC dan HP</h5>
-    <p class="card-text">SMK SWASTA YAPIM SIAK HULU</p>
-    <a href="#" class="btn btn-primary">Beli</a>
-  </div>
-</div>
-
-<div class="card" style="width: 18rem;">
-  <img src="https://sekolahpedia.id/medias/produk/tbsp4.jpeg" class="card-img-top" alt="Ulat German">
-  <div class="card-body">
-    <h5 class="card-title">Ulat German</h5>
-    <p class="card-text">SMK SWASTA YAPIM SIAK HULU</p>
-    <a href="#" class="btn btn-primary">Beli</a>
-  </div>
-</div>
-
-<div class="card" style="width: 18rem;">
-  <img src="https://sekolahpedia.id/medias/produk/wxfx9.jpeg" class="card-img-top" alt="Talam Ubi Lumer">
-  <div class="card-body">
-    <h5 class="card-title">Talam Ubi Lumer</h5>
-    <p class="card-text">SMK SWASTA YAPIM SIAK HULU</p>
-    <a href="#" class="btn btn-primary">Beli</a>
-  </div>
-</div>
-</div>
-</div>
 
 @endsection
