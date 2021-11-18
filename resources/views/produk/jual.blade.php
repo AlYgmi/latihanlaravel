5bee9da8bb5b320c64c87e84ad876e09a0102a11
AlYgmi <nurrusulialadiyat@gmail.com>
2021-11-18 09:45:02 +0700

    commit

--- resources/views/produk/jual.blade.php
+++ resources/views/produk/jual.blade.php
@@ -9,7 +9,7 @@
 <div class="container">
 <div class="row">
   <div class="card" style="width: 18rem;">
-    <img src="images.jpeg" class="card-img-top" alt="...">
+    <img src="images.jpeg" class="card-img-top">
     <div class="card-body">
       <h5 class="card-title">Pengganti botol plastik</h5>
       <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos officia quibusdam, quos facilis consequatur magni aperiam perferendis tempora consectetur, reiciendis fugiat id atque nam placeat odio inventore rem quisquam quidem?      
