4bf0aec60622461f550ef84de6450d18c9ef0352
AlYgmi <nurrusulialadiyat@gmail.com>
2021-11-23 11:56:46 +0700

    lanjutan[1]

--- resources/views/produk/detail.blade.php
+++ resources/views/produk/detail.blade.php
@@ -13,8 +13,23 @@
       <div class="row">
         
         <div class="col mt-4">
-          
-          <img src="images.jpeg" alt="">
+          <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+          <img src="images.jpeg" class="d-block w-100" alt="">
+        </div>
+        <div class="carousel-item">
+          <img src="botolkaca.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-50" alt="...">
+        </div>
+
+      </div>
+          </div>
+      </div>
+      </div>
+    </div>
         </div>
       <div class="col mt-4">
         <h3>Pengganti Botol Plastik</h3>
@@ -36,8 +51,20 @@
         </td>
       </tr>
     </table>
-    <div class="border border-5 mt-5 pb-3 pt-3 aa">
-          
+    <h3>Hubungi nomor dibawah </h3>
+    <div class="border border-5 mt-3 pb-3 pt-3 aa">
+      <div class="row">
+        <div class="col">
+          <div class="container">
+            <h3>via Whatsapp</h3>
+            +62923847923740
+          </div>
+          </div>
+          <div class="col mt-3">
+            <a href="web.whatsapp.com">hubungi sekarang!</a>
+          </div>
+          </div>
+        </div>
     </div>
       </div>
         </div>
