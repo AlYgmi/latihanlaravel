7555306c2a717f67babcf1e22bda6ed754cf8fe1
AlYgmi <nurrusulialadiyat@gmail.com>
2021-11-23 09:53:13 +0700

    lanjutan

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="shortcut icon" href="https://sekolahpedia.id/template/images/favicon.ico">
+    <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Situs Pencarian Sekolah Terbaik di Indonesia | Sekolahpedia </title>
 
@@ -35,9 +37,48 @@
           </form>
         
       </nav>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2800.8261610124573!2d107.54011166400105!3d-6.894629353954426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sbitc%20baros!5e0!3m2!1sid!2sid!4v1637127970922!5m2!1sid!2sid" width="1295" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2800.8261610124573!2d107.54011166400105!3d-6.894629353954426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sbitc%20baros!5e0!3m2!1sid!2sid!4v1637127970922!5m2!1sid!2sid" width="1295" height="450" style="border:0;" allowfullscreen="" loading="lazy" class="container-fluid"></iframe>
+
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="container">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="row">
+              <div class="col-4">
+            <div class="card rounded-circle cri" style="height: 200px; width: 200px; background-color:blue;"><div class="text-center" style="margin-top: 60px; font-size: 60px; font-family: arial; font-weight: bold; color: #fff;">187</div></div>
+          </div>
+          <div class="col-4">
+
+          <div class="carousel-item">
+            <div class="card rounded-circle cri" style="height: 200px; width: 200px;">187</div>
+          </div>
+        </div>
+          <div class="carousel-item">
+            <div class="card rounded-circle cri" style="height: 200px; width: 200px;">187</div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      </div>
+    </div>
+      </div>
 
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+<script>$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});</script>
 </body>
 </html>
--- resources/views/produk/detail.blade.php
+++ resources/views/produk/detail.blade.php
@@ -9,12 +9,41 @@
 
 @section('contrast')
   <div class="container">
-    <table class="table table-striped">
-    <tr>
-        <td>
-           asa
+    <div class="mt">
+      <div class="row">
+        
+        <div class="col mt-4">
+          
+          <img src="images.jpeg" alt="">
+        </div>
+      <div class="col mt-4">
+        <h3>Pengganti Botol Plastik</h3>
+    <table class="table table-striped mt-4">
+      <th>Harga</th>
+      <td>:</td>
+      <td>400.000</td>
+      <tr>
+       <th>Asal</th>
+        <td>:</td>
+        <td>SMKN 13 Bandung</td> 
+      </tr>
+      <tr>
+        <th>Deskripsi</th>
+        <td>:</td>
+        <td>bosan dengan botol plastik yang tidak efisien?
+          memperkenalkan botol pengganti plastik
+          mengandung 100% kaca 
         </td>
-    </tr>
-  </table>
+      </tr>
+    </table>
+    <div class="border border-5 mt-5 pb-3 pt-3 aa">
+          
+    </div>
+      </div>
+        </div>
+        <div class="text-center mt">
+        <a href="/produk">kembali ke produk</a>
+    </div>
+  </div>
 </div>  
 @endsection
